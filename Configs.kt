598a010f004cf9859f7cf6374d25c024f1da41fa
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-05-23 05:12:56 -0400

    update API to official CurseForge API, added config location for curseForgeAPIKey

---
+++ Configs.kt
@@ -0,0 +1,92 @@
+package atm.bloodworkxgaming.serverstarter.config
+
+import java.util.*
+
+data class AdditionalFile(
+    var url: String = "",
+    var destination: String = ""
+)
+
+data class LocalFile(
+    var from: String = "",
+    var to: String = ""
+)
+
+data class ModpackConfig(
+    var name: String = "",
+    var description: String = ""
+)
+
+data class LaunchSettings(
+    var spongefix: Boolean = false,
+    var ramDisk: Boolean = false,
+    var checkOffline: Boolean = false,
+    var checkUrls: List<String> = Collections.emptyList(),
+    var maxRam: String = "",
+    var minRam: String = "",
+
+    var startFile: String = "",
+    var startCommand: List<String> = Collections.emptyList(),
+    var javaArgs: List<String> = Collections.emptyList(),
+    var autoRestart: Boolean = false,
+    var crashLimit: Int = 0,
+    var crashTimer: String = "",
+    var preJavaArgs: String = "",
+
+    var forcedJavaPath: String = "",
+
+    var supportedJavaVersions: List<String> = Collections.emptyList()
+
+    ) {
+    val processedForcedJavaPath: String
+        get() {
+            var str = forcedJavaPath
+            val regex = Regex("\\\$\\{(.+)}")
+            for (matchResult in regex.findAll(forcedJavaPath)) {
+                val res = matchResult.groupValues.getOrNull(0) ?: continue
+                val inner = matchResult.groupValues.getOrNull(1) ?: continue
+                str = str.replace(res, System.getenv(inner))
+            }
+
+            return str
+        }
+}
+
+data class InstallConfig(
+    var mcVersion: String = "",
+
+    var loaderVersion: String = "",
+    var installerUrl: String = "",
+    var installerArguments: List<String> = Collections.emptyList(),
+
+    var modpackUrl: String = "",
+    var modpackFormat: String = "",
+    var curseForgeAPIKey: String = "",
+    var formatSpecific: Map<String, Any> = Collections.emptyMap(),
+
+    var baseInstallPath: String = "",
+    var ignoreFiles: List<String> = Collections.emptyList(),
+    var additionalFiles: List<AdditionalFile> = Collections.emptyList(),
+    var localFiles: List<LocalFile> = Collections.emptyList(),
+
+    var checkFolder: Boolean = false,
+    var installLoader: Boolean = false,
+
+    var spongeBootstrapper: String = "",
+    var connectTimeout: Long = 30,
+    var readTimeout: Long = 30,
+) {
+
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> getFormatSpecificSettingOrDefault(name: String, fallback: T?): T? {
+        return formatSpecific.getOrDefault(name, fallback) as T?
+    }
+}
+
+data class ConfigFile(
+    var _specver: Int = 0,
+    var modpack: ModpackConfig = ModpackConfig(),
+    var install: InstallConfig = InstallConfig(),
+    var launch: LaunchSettings = LaunchSettings()
+)
---
+++ CurseIDPackType.kt
@@ -0,0 +1,15 @@
+package atm.bloodworkxgaming.serverstarter.packtype.curse
+
+import atm.bloodworkxgaming.serverstarter.InternetManager
+import atm.bloodworkxgaming.serverstarter.config.ConfigFile
+
+class CurseIDPackType(configFile: ConfigFile, internetManager: InternetManager) : CursePackType(configFile, internetManager) {
+    override fun cleanUrl(url: String): String {
+        val split = url.split(":")
+        if (split.size != 2) {
+            throw RuntimeException("In CurseID Format the ID has to be given in format {packid}:{fileid}")
+        }
+
+        return "https://minecraft.curseforge.com/projects/${split[0]}/files/${split[1]}/download"
+    }
+}
